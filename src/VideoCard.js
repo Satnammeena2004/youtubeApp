224cac52eec363e566401985838ad8730abbbc4c
Satnammeena2004 <satnam.meena2004@gmail.com>
2024-07-30 22:07:55 +0530

    Update Tailwind CSS classes to enhance layout and responsiveness

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -38,12 +38,15 @@ function VideoCard({ data }) {
   return (
     <Link to={`/watch?v=${id?.videoId ?? id}`}>
       <div className="  p-2 rounded-lg *:text-sm flex flex-col m-2 min-h-80 text-[#f1f1f1]">
-        <div className="  border-slate-500 rounded-md flex justify-center items-center overflow-hidden min-h-44">
-          <img alt="video" src={thumbnails?.medium?.url} className="rounded-md scale-y-105 w-full" />
+        <div className=" border-slate-500 rounded-2xl  flex justify-center items-center max-h-52 overflow-hidden min-h-44">
+          <img alt="video" src={thumbnails?.high?.url} className="rounded-md scale-y-105  min-h-64" />
         </div>
         <div className='flex'>
-          <div className='min-w-10 relative'>
+          <div className='min-w-10 relative '>
+            <div>
+
             <img className="rounded-full absolute top-4  w-8 h-8 float-left" src={thumbnails?.high?.url} alt="thubnail" />
+            </ div>
 
           </div>
           <div className="flex flex-col gap-y-2 h-1/2 pb-3 py-2 ">
