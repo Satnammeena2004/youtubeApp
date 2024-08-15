36a726af655a75403efef2a3fc4ab4608c44f3b1
Satnammeena2004 <satnam.meena2004@gmail.com>
2024-08-15 23:17:45 +0530

    category wise searching and routing added

--- src/Category.js
+++ src/Category.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { VIDEO_CATEGORY_API_URL, YOUTUBE_API_KEY } from './constant';
 import { IoIosArrowForward } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
 // const category = ["Music","Cricket","Comedy","Movie","Secns","Satnd up","Video","Trendig","Cricket","Cricket","Cricket"]
 
 
 
 function Category() {
 const [category,setCategory] = useState([])
+const  navigate = useNavigate();
 
-
- function handleCLick(){
-  console.log("CLicked");
+ function handleCLick(to){
+  navigate("/search?q="+to?.toLowerCase());
  }
   
 
@@ -40,7 +41,7 @@ const [category,setCategory] = useState([])
         })
       }} className='fixed right-4'><IoIosArrowForward className='text-xl text-white bg-black '/></button>
       {
-        category.map((category)=><button key={category.id} onClick={handleCLick} className='px-2 py-1 text-[#f1f1f1] rounded-md mx-2 text-sm break-keep text-nowrap bg-[#272727]'>{category.snippet.title}</button>)
+        category.map((category)=><button key={category.id} onClick={()=>handleCLick(category.snippet.title)} className='px-2 py-1 text-[#f1f1f1] rounded-md mx-2 text-sm break-keep text-nowrap bg-[#272727]'>{category.snippet.title}</button>)
       }
        
   
